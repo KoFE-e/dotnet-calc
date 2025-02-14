2d5ec9cf1d0949e4410fd556cce477635196eb6f
Evgeny Petrovyh <petrovykh03@gmail.com>
2025-02-14 13:48:31 +0300

    init: initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+int b;
+
+String[] Menu = ["1) ввести A",
+                 "2) ввести B",
+                 "3) выполнить операцию '+'",
+                 "4) выполнить операцию '-'",
+                 "5) выполнить операцию '*'",
+                 "6) выполнить операцию '/'"];
+
+
+void ShowMenu() {
+    foreach (string option in Menu) {
+        Console.WriteLine(option);
+    }
+}
+
+ShowMenu();
